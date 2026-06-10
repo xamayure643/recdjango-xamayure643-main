3693fcea8fb1f456f5e4ccd699cb049bf0d2ad70
Alejandro <xamayure643@ieshnosmachado.org>
2026-06-10 19:39:32 +0200

    ADD: Estilos base CSS provisionales

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 LOGIN_REDIRECT_URL = 'juegos:lista'
 LOGOUT_REDIRECT_URL = 'juegos:lista'
